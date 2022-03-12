35008a6025ea0c4c562c19e4a4d56b030f13c100
Sönke Peters <soenke@soenkep.com>
2022-03-12 17:07:17 +0100

    add read/write functionality

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,12 +1,15 @@
-import { json, useLoaderData } from "remix";
-import { Redis } from "@upstash/redis";
-
-const redis = Redis.fromCloudflareEnv();
+import { json, redirect, useLoaderData } from "remix";
+import redis from "../utils/redis.server";
 
 export const loader = async () => {
   const start = new Date();
-  const count = await redis.incr("counter");
+  const count = await redis.get("counter");
   return json({ count, loadingTime: new Date() - start });
+};
+
+export const action = async () => {
+  await redis.incr("counter");
+  return redirect("/");
 };
 
 export default function Index() {
@@ -15,21 +18,18 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Record speeds with Remix on Cloudflare and Upstash Redis</h1>
+      <p>The button below was clicked {count} times already.</p>
+      <form method="post" action="/?index">
+        <button type="submit">Click me!</button>
+      </form>
       <p>
-        In the last <b>{loadingTime} ms</b> this app ...
+        It took <b>{loadingTime} ms</b> to read the number of button clicks from{" "}
+        <a href="https://upstash.com/redis">Upstash Redis</a>{" "}
+        <a href="https://docs.upstash.com/redis/features/globaldatabase">
+          Global Database
+        </a>
+        .
       </p>
-      <ul>
-        <li>
-          incremented the page view counter on an{" "}
-          <a href="https://upstash.com/redis">Upstash Redis</a>{" "}
-          <a href="https://docs.upstash.com/redis/features/globaldatabase">
-            Global Database
-          </a>
-        </li>
-        <li>fetched the current page view count</li>
-      </ul>
-
-      <p>This was done {count} times already.</p>
     </div>
   );
 }
